82218bb77d8b35a3966d9e9e2533acfa54b57dea
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-06-25 20:33:06 +0200

    feat: create question result component

---
+++ src/components/QuestionResult.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Badge from "./Badge";
+
+const QuestionResult = ({ options, answer }) => {
+  const optionKeys = Object.keys(options);
+  let totalVotes = 0;
+  optionKeys.forEach((key) => {
+    totalVotes += options[key].votes.length;
+  });
+  return (
+    <div className='question-result'>
+      <h3>Results:</h3>
+      {optionKeys.map((key) => (
+        <Badge key={key} badgeContent={answer === key ? "your vote" : null}>
+          <div
+            className={`question-option ${
+              answer === key ? "option-voted" : ""
+            }`}
+          >
+            <p>Would you rather {options[key].text}</p>
+            <p className='option-result center'>
+              <span>
+                {parseFloat(
+                  ((options[key].votes.length * 100) / totalVotes).toFixed(1)
+                ).toString()}
+                <span className='option-result-percent'>%</span>
+              </span>
+              <span className='text-small default-color'>{`${options[key].votes.length} out of ${totalVotes}`}</span>
+            </p>
+          </div>
+        </Badge>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionResult;
